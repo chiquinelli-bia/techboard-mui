c8463407f0c112d215c4d89edb6f623adb98496d
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-08-19 10:11:37 -0300

    feat(app): importa themeProvider e adiciona cssBaseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import { Board } from "./components/board";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material";
+import theme from "../theme";
 
 function App() {
-  return <></>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Board />
+    </ThemeProvider>
+  );
 }
 
 export default App;
