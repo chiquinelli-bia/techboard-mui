890c0573d39487cb5fc0d7ee84f9d8c4fa994a92
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-08-26 10:15:02 -0300

    feat(App): configura tanStack

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { Board } from "./components/board";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material";
 import theme from "../theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Board />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Board />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
